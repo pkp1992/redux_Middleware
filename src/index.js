9395e0206f7d3d64e4abd7f9e4cf2b0d1adc659c
pkp1992 <30792296+pkp1992@users.noreply.github.com>
2019-05-22 12:36:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-// lesson 13 time: 01:17:05
